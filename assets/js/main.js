d1cdf50265c5a32d2e0c868d1022e65b60be122b
maia rojas <maia.rt.46@gmail.com>
2017-03-10 08:22:53 -0500

    Subiendo avances

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 function saludo (){
   var name = prompt("¿Quién es usted?");
 }
-saludo();
+
+//SPRINTS
+var mostarOcultar = function(divtexte) {
+	var text1 = document.getElementById(divtexte);
+	var text1 = document.getElementById(divtexte);
+	var text1 = document.getElementById(divtexte);
+	
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
